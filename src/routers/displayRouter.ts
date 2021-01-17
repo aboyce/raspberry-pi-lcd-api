b2d0bcf396fd81a5bafe14ed4311482270322c45
aboyce <adam@aboyce.uk>
2021-01-17 14:55:43 +0000

    Fixed swagger links and the router not passing onto the next middleware.

--- src/routers/displayRouter.ts
+++ src/routers/displayRouter.ts
@@ -8,37 +8,40 @@ const router = express.Router()
 
 /**
  * @swagger
- * /info:
+ * /display/info:
  *   get:
  *     description: Get basic informaton about the connected display
  */
-router.get('/info', async (req, res) => {
+router.get('/info', async (req, res, next) => {
   const lcd: Display = res.locals.display
   res.json(await lcd.getInfo())
+  next()
 })
 
 /**
  * @swagger
- * /on:
+ * /display/on:
  *   get:
  *     description: Turn the LCD display on
  */
-router.get('/on', async (req, res) => {
+router.get('/on', async (req, res, next) => {
   const lcd: Display = res.locals.display
   await lcd.on()
   res.send('on')
+  next()
 })
 
 /**
  * @swagger
- * /off:
+ * /display/off:
  *   get:
  *     description: Turn the LCD display off
  */
-router.get('/off', async (req, res) => {
+router.get('/off', async (req, res, next) => {
   const lcd: Display = res.locals.display
   await lcd.off()
   res.send('off')
+  next()
 })
 
 export default router
