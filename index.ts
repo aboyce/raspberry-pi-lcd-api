d26bc985794406a0380e4e8e6bc4e8cbfa6dc045
aboyce <adam@aboyce.uk>
2021-01-17 18:05:46 +0000

    Added body parsing and cors.

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 // Libraries
 import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
 import swaggerUi from 'swagger-ui-express'
 import swaggerJSDoc from 'swagger-jsdoc'
 
@@ -11,6 +13,10 @@ import swaggerOptions from './swagger'
 
 const app = express()
 const port = 3000
+
+app.use(bodyParser.json())
+
+app.use(cors())
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerJSDoc(swaggerOptions)))
 
