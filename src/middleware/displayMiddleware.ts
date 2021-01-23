5ee100777c214862b0fba24c84db87652f9e4b7d
aboyce <adam@aboyce.uk>
2021-01-23 17:05:59 +0000

    Updated logging.

--- src/middleware/displayMiddleware.ts
+++ src/middleware/displayMiddleware.ts
@@ -7,11 +7,11 @@ import Display from '../domain/display'
 //const lcd = new Display(1, 63, 16, 2)
 //const lcd = new Display(1, 63, 20, 4)
 
-export const displayConnectMiddleware: RequestHandler = async (req, res, next): Promise<void> => {
+export const displayConnectMiddleware: RequestHandler = (req, res, next): void => {
   try {
     const display = new Display(1, 63, 20, 4)
-    await display.isConnected()
-    console.log('Connected to Display')
+    display.testConnection()
+    console.log('Middleware: Connected to Display')
     res.locals.display = display
   } catch (error) {
     next(error)
@@ -19,11 +19,11 @@ export const displayConnectMiddleware: RequestHandler = async (req, res, next): 
   next()
 }
 
-export const displayDisconnectMiddleware: RequestHandler = async (req, res, next): Promise<void> => {
+export const displayDisconnectMiddleware: RequestHandler = (req, res, next): void => {
   try {
     const lcd: Display = res.locals.display
-    await lcd.close()
-    console.log('Closed connection to Display')
+    lcd.close()
+    console.log('Middleware: Closed connection to Display')
   } catch (error) {
     next(error)
   }
