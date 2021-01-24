816060bc24e2e992a4d93c6789f1215207243d5c
aboyce <adam@aboyce.uk>
2021-01-24 14:06:19 +0000

    Improved count formatting.

--- src/routers/modules/npmRouter.ts
+++ src/routers/modules/npmRouter.ts
@@ -32,7 +32,7 @@ router.get('/downloads/weekly', async (req, res, next) => {
     const lcd: Display = res.locals.display
     lcd.reset()
     lcd.printLine(0, npmPackage.name)
-    lcd.printLine(1, `Weekly: ${result.downloads}`)
+    lcd.printLine(1, `Weekly: ${result.downloads.toLocaleString()}`)
     if (lcd.hasExtraRows()) {
       lcd.printLine(2, `Start: ${result.start}`)
       lcd.printLine(3, `End: ${result.end}`)
